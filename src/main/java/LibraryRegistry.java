3c5b83e55f3ece1087fd2d42383917c4cc731706
NSergeevich87 <niknikitaser@gmail.com>
2024-04-30 15:53:26 +0200

    test2_start

--- src/main/java/LibraryRegistry.java
+++
@@ -1,6 +0,0 @@
-import java.util.List;
-public interface LibraryRegistry {
-    void add(AbstractItem item);
-    AbstractItem findByInventoryNumber(String inventoryNumber);
-    List<AbstractItem> findByAuthor(String author);
-}
